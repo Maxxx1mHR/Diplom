06b618299e8a1c85e8a79a5ff07585d0dc3e1375
Maxim <maksim.pudeev@bk.ru>
2022-02-08 16:27:09 +0300

    add app-routing

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {CreateComponent} from "../create/create.component";
+import {ReadComponent} from "../read/read.component";
 
+const routes: Routes = [
+  {path: 'create' ,component:CreateComponent},
+  {path: 'read', component:ReadComponent}
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
