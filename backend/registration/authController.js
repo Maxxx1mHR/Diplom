a312889b18837e7847e1c255913f82e47e3e1ffa
Your Name <you@example.com>
2022-04-20 22:21:13 +0300

    doing 2nd version registratiob

--- backend/registration/authController.js
+++ backend/registration/authController.js
@@ -1,37 +1,55 @@
 const {Staff} = require("./index");
-//import {Staff} from "./index";
-const bodyparser = require('body-parser');
-const express = require("express");
-const cors = require("cors");
-//const registration = require('./registration')
-const app = express();
-app.use(cors());
-app.use(bodyparser.json());
+const bcrypt = require('bcrypt')
+const {validationResult} = require('express-validator')
 
-class authController{
+class authController {
 
-   async registration(req,res) {
-       try {
-           const login = req.body.login
-           const password = req.body.password
-           Staff.findOne({where: {login: login}}).then((candidate) => {
-               if (candidate) {
-                   return res.status(400).json('Ошибка уже существует')
-               }
-               //console.log("Пользователь добавлен")
-               const staff = new Staff({login, password})
-               staff.save()
-               res.status(400).json({message: 'Пользователь добавлен'})
-           })
+    async registration(req, res) {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: "Ошибка при регистрации", errors})
+            }
+            const login = req.body.login
+            const password = req.body.password
+            Staff.findOne({where: {login: login}}).then((candidate) => {
+                if (candidate) {
+                    return res.status(400).json({message: 'Ошибка уже существует'})
+                }
+                //console.log("Пользователь добавлен")
+                //Добавить синхронизацию с БД???
+                const hasPassword = bcrypt.hashSync(password, 10)
+                const staff = new Staff({login, hasPassword})
+                //staff.save()
+                res.status(400).json({message: 'Пользователь добавлен'})
+            })
 
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'Не удалось'})
+        }
+    }
 
-       } catch (e) {
-           console.log(e)
-           res.status(400).json({message: 'Не удалось'})
-       }
+    async login(req, res) {
+        try {
+            const login = req.body.login
+            const password = req.body.password
+            Staff.findOne({where: {login: login}}).then((user) => {
+                if (!user) {
+                    return res.status(400).json({message: `Пользователь ${user} не найден`})
+                }
+                const validPassword = bcrypt.compareSync(password, user.password)
+                //console.log(user.password)
+                if(!validPassword){
+                    return res.status(400).json({message: 'Введен неверный пароль'})
+                }
+            })
 
-   }
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'Не удалось'})
+        }
+    }
 }
 
-
-module.exports = new authController()
+    module.exports = new authController()
--- backend/registration/authRouter.js
+++ backend/registration/authRouter.js
@@ -1,9 +1,14 @@
 const Router = require ('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration', controller.registration)
-router.post('/login')
+router.post('/registration', [
+    check('login',"Логи не может быть пустым").notEmpty(),
+    check('password', "Пароль должен быть больше 6").isLength({min:6})
+
+    ], controller.registration)
+router.post('/login', controller.login)
 router.get('/staff')
 
 module.exports =router
